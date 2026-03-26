449b8f8282ce06adece2fad411d30602e77b744b
Ledger Dare <Lpanda909@gmail.com>
2026-03-26 15:55:18 -0700

    Added Comments to movement script

--- player/player_3d.gd
+++ player/player_3d.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 @onready var camera: Camera3D = get_viewport().get_camera_3d()
 var movement_basis: Basis
 
+# Global paramaters for character movement
 const SPEED = 3
 const ROTATION_SPEED = 2.7
 const JUMP_VELOCITY = 4.5
@@ -18,13 +19,14 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	
+	# sets camera bias to current 3d camera
 	if camera != get_viewport().get_camera_3d():
 		camera = get_viewport().get_camera_3d()
 		movement_basis = camera.global_transform.basis
 	
+	# when not moving, slow velocity towards zero vector
 	if input_dir == Vector2.ZERO:
 		velocity = velocity.move_toward(Vector3.ZERO, SPEED * delta * 2)
 	else:
@@ -32,25 +34,31 @@ func _physics_process(delta: float) -> void:
 		if movement_basis == Basis():
 			movement_basis = camera.global_transform.basis
 		
+		# calculate facing direction of camera and assign it to movement_basis
 		var camera_forward = -movement_basis.z
 		var camera_right = movement_basis.x
 		camera_forward.y = 0
 		camera_forward = camera_forward.normalized()
 		
+		# get move direction by adding input movement to the base camera
 		var move_direction = (camera_forward * -input_dir.y + camera_right * input_dir.x)
 		move_direction = move_direction.normalized()
 		
+		# get the forward facing direction of the player character
 		var character_forward = Vector3(sin(rotation.y), 0, cos(rotation.y))
 		
+		# when moving, rotate the charater towards the desired
+		# location (reduces "snapping" and makes movement smoother)
 		if move_direction.length() > 0.1:
 			var target_angle = atan2(move_direction.x, move_direction.z)
 			rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
 		
-		
+		# Apply velocity to character
 		velocity.x = character_forward.x * SPEED
 		velocity.z = character_forward.z * SPEED
 		
 	
+	# When attempting to move, change animation to walking
 	if velocity.length() > 0.1:
 		$AnimationTree["parameters/playback"].travel("Walk Cycle")
 	else:
